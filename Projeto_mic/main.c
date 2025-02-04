66f35169f0823aa20ed4f82341674b754bca9ed4
edivaldob22 <edivaldo.b22@aluno.ifsc.edu.br>
2025-02-04 10:43:21 -0300

    Commit inicial - estrutura

---
+++ Projeto_mic/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 2/4/2025 9:19:04 AM
+ *  Author: Edivaldo
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
