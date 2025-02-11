1d809defa680ebefb8e77e2d8899068bee5efaac
edivaldob22 <edivaldo.b22@aluno.ifsc.edu.br>
2025-02-11 10:23:44 -0300

    Adicionado estrutura para funções do display

---
+++ Projeto_mic/disp.c
@@ -0,0 +1,9 @@
+/*
+ * teclado.c
+ *
+ * Created: 11/02/2025 11:15:06
+ *  Author: Edivaldo
+ */ 
+#include "disp.h"
+
+//desenvolver as funções do display
---
+++ Projeto_mic/disp.h
@@ -0,0 +1,16 @@
+/*
+ * disp.h
+ *
+ * Created: 11/02/2025 10:16:40
+ *  Author: ediva
+ */ 
+
+
+#ifndef DISP_H_
+#define DISP_H_
+
+//declarar a(s) funções do display
+
+
+
+#endif /* DISP_H_ */
