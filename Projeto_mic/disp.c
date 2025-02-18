c9ed34056718561353d1040d7e804b2c3927c201
edivaldob22 <edivaldo.b22@aluno.ifsc.edu.br>
2025-02-18 09:21:28 -0300

    definição das funções do display, header e c

--- Projeto_mic/disp.c
+++ Projeto_mic/disp.c
@@ -1,9 +1,23 @@
 /*
- * teclado.c
+ * disp.c
  *
  * Created: 11/02/2025 11:15:06
  *  Author: Edivaldo
  */ 
 #include "disp.h"
-
-//desenvolver as funções do display
+void disp_inicial()
+{
+	
+}
+void disp_clear()
+{
+	
+}
+void disp_set(int linha, int col)
+{
+	
+}
+void disp_w(char msg[16])
+{
+	
+}
--- Projeto_mic/disp.h
+++ Projeto_mic/disp.h
@@ -2,7 +2,7 @@
  * disp.h
  *
  * Created: 11/02/2025 10:16:40
- *  Author: ediva
+ *  Author: Edivaldo
  */ 
 
 
@@ -10,7 +10,10 @@
 #define DISP_H_
 
 //declarar a(s) funções do display
-
+void disp_inicial();
+void disp_clear();
+void disp_set(int linha, int col);
+void disp_w(char msg[16]);
 
 
 #endif /* DISP_H_ */
