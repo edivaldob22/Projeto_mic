2b05c4188e9d17fc4bed9d21457da18a2ff98687
edivaldob22 <edivaldo.b22@aluno.ifsc.edu.br>
2025-02-25 08:44:01 -0300

    Atualização do main com a leitura da senha, verificação da senha correta e escrita no display

--- Projeto_mic/main.c
+++ Projeto_mic/main.c
@@ -2,26 +2,31 @@
  * main.c
  *
  * Created: 2/4/2025 9:19:04 AM
- *  Author: Guilherme
- */ 
+ *  Author: Edivaldo e Guilherme
+ */
 
-<<<<<<< HEAD
 #include <xc.h>
-#include <util/delay.h>
-#include <avr/interrupt.h>
-=======
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
->>>>>>> 229071333787caca1ed473632468f1d336671f1f
+#include <stdio.h>
+#include <string.h>
 #include "teclado.h"
+#include "disp.h"
 
-<<<<<<< HEAD
 #define F_CPU 8000000
+
+#define tam_Senha 6
+
+char senha_correta[7] = "123456";
+int senhaCorretaFlag = 0;
+int ax = 0;
+int f_key = 0;
+char armazenaKey[7];  // Armazena até 6 teclas
 
 int main(void)
 {
-	DDRC = 0b00111110; //PC1 Buzzer PC2 a 5 disp
+	DDRC = 0b00111111; //PC1 Buzzer PC2 a 5 disp
 	DDRB = 0b00000011; //PB2 a 5 entrada teclado (coluna)
 	DDRD = 0b11111111; //PD0 a 5 disp
 	//PB0 e 1 e PD6 e 7 saida teclado.
@@ -29,42 +34,70 @@ int main(void)
 	PORTB = 0x00;
 	PORTC = 0x00;
 	PORTD = 0x00;
+
+	setupKeypad();
+	setupInterrupts();
 	
 	disp_inicial();
 	
-    while(1)
-    {
-		//
-    }
-=======
-#define F_CPU 8000000  // Frequência do microcontrolador
-
-volatile char lastKeyPressed = '\0';
-char armazenaKey[6] = "";  // Armazena até 6 teclas
-int keyIndex = 0;
-
-int main() {
 	while (1) {
-		if (lastKeyPressed != '\0') {
-			// Armazena a tecla pressionada
-			if (keyIndex < 6) {
-				armazenaKey[keyIndex++] = lastKeyPressed;
-				armazenaKey[keyIndex] = '\0'; // Mantém a string terminada corretamente
+		char mensagem[16] = "Digite a Senha";
+		
+		if (ax == 0) {
+			ax = 1;
+			disp_set(1);
+			disp_w(mensagem, 14);      
+			int i = 0;
+			while (i < tam_Senha) {
+				char tecla = scanKeypad();
+
+				if (tecla != '\0') {
+					armazenaKey[i++] = tecla; // Armazena a tecla no vetor
+					armazenaKey[i] = '\0';    // Finaliza a string
+
+					disp_set(2);              // Define a segunda linha do display
+					disp_w(armazenaKey, i);   // Exibe a senha digitada
+					_delay_ms(800);
+				}
+			
 			}
-			lastKeyPressed = '\0';  // Limpa a variável após uso
+			if(i == tam_Senha) f_key = 1;
 		}
-		if (armazenaKey[0] == '1') {
-		PORTB |= 0b00000010;
-		_delay_ms(1000);
-		PORTB &= ~0b00000010;
+		
+		if(f_key == 1){
+			for (int i = 0; i < tam_Senha; i++) {
+				if ((armazenaKey[i]) != (senha_correta[i])) senhaCorretaFlag++;
+			}
+
+			if(senhaCorretaFlag > 0) {
+				//beepLongo();
+				disp_clear();
+				disp_set(1);
+				strcpy(mensagem, "Senha incorreta");
+				disp_w(mensagem,15);
+				_delay_ms(2000);
+				disp_clear();
+				ax = 0;
+				f_key = 0;
+				senhaCorretaFlag = 0;
+			}
+			else {
+				//beepCorreto();
+				disp_clear();
+				disp_set(1);
+				strcpy(mensagem, "Senha correta");
+				disp_w(mensagem,13);
+				disp_set(2);
+				strcpy(mensagem, "Cofre aberto");
+				disp_w(mensagem,12);
+				_delay_ms(1000);
+			}
 		}
 	}
-
+	
 	return 0;
 }
-
-// Rotina de interrupção para detectar pressionamento de teclas
 ISR(PCINT0_vect) {
-	lastKeyPressed = scanKeypad();
->>>>>>> 229071333787caca1ed473632468f1d336671f1f
+	
+	PORTC ^= 0b00000001;
 }
